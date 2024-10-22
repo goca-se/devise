fb60e608d25a3202b54a46cf375c1c4032156264
Vitor Rosa <vitor.rosa@gocase.com>
2024-10-22 17:57:27 -0300

    fix: removing omni auth version regex

--- lib/devise/omniauth.rb
+++ lib/devise/omniauth.rb
@@ -8,10 +8,6 @@ rescue LoadError
   raise
 end
 
-unless OmniAuth::VERSION =~ /^1\./
-  raise "You are using an old OmniAuth version, please ensure you have 1.0.0.pr2 version or later installed."
-end
-
 # Clean up the default path_prefix. It will be automatically set by Devise.
 OmniAuth.config.path_prefix = nil
 
